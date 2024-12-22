7d10309903f03c4e8c6beb62ee3bb1aa0ce0c8ac
Gregory Leblanc <headmaster.albus.dumbledore+github@gmail.com>
2024-12-21 22:12:18 -0500

    Add emdash to allowed characters.
    I had an audiobook where this came out weird in the filenames.  Adding emdash seemed the easiest thing to do.  I also removed the space before the dash, because spaces in filenames make life more difficult than it needs to be.  I could also rewrite this to replace the emdash with a dash.

--- chrip.js
+++ chrip.js
@@ -10,7 +10,7 @@ function sleep(ms) {
 }
 
 function filename(name) {
-    return name.replaceAll('&', 'and').replaceAll(':', ' -').replaceAll(/[^a-z0-9 ._-]+/ig, '');
+    return name.replaceAll('&', 'and').replaceAll(':', '-').replaceAll(/\s+/g, '_').replaceAll(/[^a-z0-9._-—]+/ig, '');
 }
 
 async function getCover(dirname, driver) {
